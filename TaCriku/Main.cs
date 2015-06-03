e4bcd21f29ba9c2dd9141fd77b1cbc0f4b68e2f0
flan <flan@flande.re>
2015-06-03 17:59:05 +0200

    cleanups

--- TaCriku/Main.cs
+++ TaCriku/Main.cs
@@ -1,22 +1,20 @@
-
 using System;
 
 namespace TaCriku
 {
-
-
 	public class Program
 	{
-
 		public static void Main ()
 		{
-			MainWindow mw = new MainWindow();
 			int count = 0;
+			MainWindow mw = new MainWindow();
 			mw.Text = count.ToString();
+
 			mw.Controls[0].Click += delegate {
 				count++;
 				mw.Text = count.ToString();
 			};
+
 			mw.ShowDialog();
 			Console.WriteLine(count);
 		}
--- TaCriku/MainWindow.cs
+++ TaCriku/MainWindow.cs
@@ -4,25 +4,22 @@ using System.Windows.Forms;
 
 namespace TaCriku
 {
-
-
 	public class MainWindow : Form
 	{
 		private Label tb = new Label();
-		
 
 		public MainWindow ()
 		{
 			this.Size = new System.Drawing.Size(120, 75);
-			tb.Dock = DockStyle.Fill;
 			this.Font = new System.Drawing.Font("Arial", 25);
+
+			tb.Dock = DockStyle.Fill;
 			tb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
 			this.Controls.Add(tb);
 			this.TextChanged += delegate {
 				tb.Text = this.Text;
 			};
-
-			
 		}
 	}
 }
