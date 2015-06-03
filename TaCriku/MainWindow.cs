bda4639c8fd17087c7846dce431ccac039a9fdf5
flan <flan@flande.re>
2015-06-03 17:32:10 +0200

    fix resizing issues

--- TaCriku/MainWindow.cs
+++ TaCriku/MainWindow.cs
@@ -14,11 +14,9 @@ namespace TaCriku
 		public MainWindow ()
 		{
 			this.Size = new System.Drawing.Size(120, 75);
-			tb.Left = 0;
-			tb.Top = 0;
-			tb.Size = new System.Drawing.Size(120, 75);
+			tb.Dock = DockStyle.Fill;
 			this.Font = new System.Drawing.Font("Arial", 25);
-			tb.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			tb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			this.Controls.Add(tb);
 			this.TextChanged += delegate {
 				tb.Text = this.Text;
